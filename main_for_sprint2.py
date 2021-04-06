15bdeb8bb7a11badb9d270ef941dc041e273b981
Hunyady Borbála <hunyadyb@localhost.localdomain>
2021-04-06 15:49:02 +0200

    created main_for_sprint2.py and made todo list according to what route it has to connect to

---
+++ main_for_sprint2.py
@@ -0,0 +1,163 @@
+#TODO: Search result list page
+@app.route('/search', methods=['GET'])
+#def list_matching():
+
+'''
+@app.route('/list', methods=['GET', 'POST'])
+def list_questions():
+    temp = connection.read_data("sample_data/question.csv")
+    sort = request.args.get("sort") if request.args.get("sort") else "submission_time"
+    order = request.args.get("order") if request.args.get("order") else "desc"
+    questions_list = data_manager.sorting_questions(temp, sort, order)
+    for item in questions_list:
+        item["converted_time"] = util.transform_timestamp(item["submission_time"])
+    return render_template('list_questions.html', questions=questions_list)
+'''
+
+#TODO: Edit answer page
+'''
+@app.route('/question/<question_id>', methods=['GET', 'POST'])
+def display_question(question_id):
+    """Routes to the specific ID of the selected question with answers"""
+    questions_list = connection.read_data("sample_data/question.csv")
+    answers_list = []
+    for answer in connection.read_data("sample_data/answer.csv"):
+        if answer['question_id'] == question_id:
+            answers_list.append(answer)
+
+    for row in questions_list:
+        if row['id'] == question_id:
+            data_manager.update_view_count(question_id)
+            return render_template('show_answers.html', question=row, answers=answers_list)
+
+    abort(404)
+'''
+
+#TODO: Edit comment page
+'''
+@app.route('/answer/<question_id>/new-answer', methods=['GET', 'POST'])
+def add_answer(question_id):
+    file = connection.read_data("sample_data/answer.csv")
+
+    id = util.create_id(file)
+    vote_number = 0
+    unix_time = time.time()
+
+    if request.method == 'GET':
+        return render_template('add_answer.html')
+    else:
+        message = request.form.get('input_message')
+        picture = request.form.get('input_image_url')
+        file.append(
+            {'id': id,
+             'submission_time': int(unix_time),
+             'vote_number': vote_number,
+             'question_id': question_id,
+             'message': message,
+             'image': picture}
+        )
+        connection.write_data(connection.ANSWER_FILE_PATH, connection.ANSWERS_HEADER, file)
+        return redirect(url_for('list_questions'))
+'''
+
+#TODO: Delete Comment page
+'''
+@app.route('/add-question', methods=['GET', 'POST'])
+def add_question():
+    file = connection.read_data("sample_data/question.csv")
+    if request.method == 'GET':
+        return render_template('ask_question.html')
+    else:
+        id = util.create_id(file)
+        unix_time = int(time.time())
+        view_number = 0
+        vote_number = 0
+        title = request.form.get('input_title')
+        message = request.form.get('input_message')
+
+        images = request.files.getlist('input_image')
+        image_list = []
+        for image in images:
+            f = image.filename
+            image.save(os.path.join("static/user-upload", f))
+            image_list.append(f)
+
+        file.append(
+            {'id': id,
+             'submission_time': unix_time,
+             'view_number': view_number,
+             'vote_number': vote_number,
+             'title': title,
+             'message': message,
+             'image': image_list
+             }
+        )
+        connection.write_data(connection.QUESTION_FILE_PATH, connection.QUESTIONS_HEADER, file)
+        return redirect(url_for('list_questions'))
+
+'''
+
+#TODO: 'add tag' page
+'''
+@app.route('/question/<question_id>/delete', methods=['POST'])
+def delete_question(question_id):
+    file = connection.read_data("sample_data/question.csv")
+    for index in range(len(file)):
+        if file[index]['id'] == question_id:
+            file.pop(index)
+            connection.write_data(connection.QUESTION_FILE_PATH, connection.QUESTIONS_HEADER, file)
+            return redirect(url_for('list_questions'))
+
+'''
+
+
+'''
+@app.route('/question/<question_id>/edit', methods=['GET', 'POST'])
+def edit_question(question_id):
+    file = connection.read_data("sample_data/question.csv")
+    for index in range(len(file)):
+        if file[index]['id'] == question_id:
+            result = file[index]
+    if request.method == 'GET':
+        return render_template('edit_question.html', result=result)
+    else:
+        unix_time = int(time.time())
+        title = request.form.get('input_title')
+        message = request.form.get('input_message')
+        image = request.form.get('input_image_url')
+        result.update({'submission_time': unix_time, 'title': title, 'message': message, 'image': image})
+        connection.write_data(connection.QUESTION_FILE_PATH, connection.QUESTIONS_HEADER, file)
+        return redirect(url_for('list_questions'))
+
+'''
+
+
+'''
+@app.route('/answer/<answer_id>/delete', methods=['POST'])
+def delete_answer(answer_id):
+    file = connection.read_data(connection.ANSWER_FILE_PATH)
+    for index in range(len(file)):
+        if file[index]['id'] == answer_id:
+            file.pop(index)
+            connection.write_data(connection.ANSWER_FILE_PATH, connection.ANSWERS_HEADER, file)
+            return redirect(url_for('list_questions'))
+
+'''
+
+'''
+@app.route('/question/<question_id>/<action>')
+# <action>: 'vote_up' or 'vote_down'
+def vote_question(question_id, action):
+    data_manager.update_question_votes(question_id, action)
+    return redirect(request.referrer)
+
+'''
+
+'''
+@app.route('/answer/<answer_id>/<action>')
+# <action>: 'vote_up' or 'vote_down'
+def vote_answer(answer_id, action):
+    data_manager.update_answer_votes(answer_id, action)
+    return redirect(request.referrer)
+
+'''
