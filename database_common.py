5bc98c3524ae15f71577acb9fc18e09c77d5aa65
Balazs Toth <tothbalazs.contact@gmail.com>
2021-04-06 18:01:13 +0200

    Change back get_connection_string variables

--- database_common.py
+++ database_common.py
@@ -5,10 +5,10 @@ import psycopg2.extras
 
 def get_connection_string():
     # Define the environment variables locally in PyCharm:
-    user_name = os.environ.get('hunyadyb')
-    password = os.environ.get('bori')
-    host = os.environ.get('localhost')
-    database_name = os.environ.get('database')
+    user_name = os.environ.get('PSQL_USER_NAME')
+    password = os.environ.get('PSQL_PASSWORD')
+    host = os.environ.get('PSQL_HOST')
+    database_name = os.environ.get('PSQL_DB_NAME')
 
     env_variables_defined = user_name and password and host and database_name
 
