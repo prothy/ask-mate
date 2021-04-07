927572cbd3eead7b2394e8df7ce816d0e73cb1d1
prothy <lukacsurgay@gmail.com>
2021-04-07 08:30:57 +0200

    questions page will show question

--- main.py
+++ main.py
@@ -26,15 +26,12 @@ def list_questions():
 @app.route('/question/<question_id>')
 def display_question(question_id):
     """Routes to the specific ID of the selected question displaying corresponding answers"""
-    question = data_manager.get_question(question_id)
+    question = data_manager.get_question(question_id)[0]
     answers_list = data_manager.get_answers_for_question(question_id)
 
-    for row in questions:
-        if row['id'] == question_id:
-            data_manager.update_view_count(question_id)
-            return render_template('show_answers.html', question=question, answers=answers_list)
+    # data_manager.update_view_count(question_id)
 
-    abort(404)
+    return render_template('show_answers.html', question=question, answers=answers_list)
 
 
 @app.route('/answer/<question_id>/new-answer', methods=['GET', 'POST'])
