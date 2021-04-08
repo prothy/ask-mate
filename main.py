a6396e126534c359d09f0586dba3faed2a80d2a4
Balazs Toth <tothbalazs.contact@gmail.com>
2021-04-08 11:30:19 +0200

    Refactor delete functions

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, redirect, render_template, url_for, abort
 from werkzeug.utils import secure_filename
 import data_manager
-import time
 import os
 
 UPLOAD_FOLDER = "/static/user-upload"
@@ -25,7 +24,7 @@ def list_questions():
 @app.route('/question/<question_id>')
 def display_question(question_id):
     """Routes to the specific ID of the selected question displaying corresponding answers"""
-    question = data_manager.get_question(question_id)[0]
+    question = data_manager.get_table_data("question", question_id)[0]
     answers_list = data_manager.get_answers_for_question(question_id)
     data_manager.update_view_count(question_id)
     return render_template('show_answers.html', question=question, answers=answers_list)
@@ -73,45 +72,34 @@ def add_question():
         return redirect(url_for('list_questions'))
 
 
-# TODO: delete_question - Needs to be refactored + SQL query
 @app.route('/question/<question_id>/delete', methods=['POST'])
 def delete_question(question_id):
-    file = connection.read_data("sample_data/question.csv")
-    for index in range(len(file)):
-        if file[index]['id'] == question_id:
-            file.pop(index)
-            connection.write_data(connection.QUESTION_FILE_PATH, connection.QUESTIONS_HEADER, file)
-            return redirect(url_for('list_questions'))
+    data_manager.delete_question(question_id)
+    return redirect(request.referrer)
 
 
-# TODO: edit_question - Needs to be refactored + SQL query
-@app.route('/question/<question_id>/edit', methods=['GET', 'POST'])
-def edit_question(question_id):
-    file = connection.read_data("sample_data/question.csv")
-    for index in range(len(file)):
-        if file[index]['id'] == question_id:
-            result = file[index]
+@app.route('/answer/<answer_id>/delete', methods=['POST'])
+def delete_answer(answer_id):
+    data_manager.delete_answer(answer_id)
+    return redirect(request.referrer)
+
+
+@app.route('/<edit_type>/<question_id>/edit', methods=['GET', 'POST'])
+def edit_question(edit_type, question_id):
+    result = data_manager.get_table_data(edit_type, question_id)
+
     if request.method == 'GET':
         return render_template('edit_question.html', result=result)
     else:
-        unix_time = int(time.time())
         title = request.form.get('input_title')
         message = request.form.get('input_message')
-        image = request.form.get('input_image_url')
-        result.update({'submission_time': unix_time, 'title': title, 'message': message, 'image': image})
-        connection.write_data(connection.QUESTION_FILE_PATH, connection.QUESTIONS_HEADER, file)
-        return redirect(url_for('list_questions'))
 
+        data_manager.update_table(edit_type, question_id, {
+            title: title,
+            message: message
+        })
 
-# TODO: delete_answer - Needs to be refactored + SQL query
-@app.route('/answer/<answer_id>/delete', methods=['POST'])
-def delete_answer(answer_id):
-    file = connection.read_data(connection.ANSWER_FILE_PATH)
-    for index in range(len(file)):
-        if file[index]['id'] == answer_id:
-            file.pop(index)
-            connection.write_data(connection.ANSWER_FILE_PATH, connection.ANSWERS_HEADER, file)
-            return redirect(url_for('list_questions'))
+        return redirect(request.referrer)
 
 
 @app.route('/question/<question_id>/<action>')
